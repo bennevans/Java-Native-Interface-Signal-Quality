fd63fe3d71a96b2b43b4e55b8c3079412f109b4b
Ben Evans <otterdude97@gmail.com>
2014-04-08 21:29:01 -0700

    added includes

---
+++ JNISignalQuality.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <tchar.h>
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+#include <wlanapi.h>
+#include <windot11.h>
+#include <objbase.h>
+#include <wtypes.h>
+#include <stdlib.h>
+
+#pragma comment(lib, "wlanapi.lib")
+#pragma comment(lib, "ole32.lib")
